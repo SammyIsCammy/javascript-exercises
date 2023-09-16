3a056d8af121cecd2fbbee9737f911effa7810d2
Samuel Camacho <mr.sammycamacho@gmail.com>
2023-09-16 17:26:38 -0400

    Verified code test files. Starting Exercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    for (let i = 0; i < num; i++) {
+        let string = string;
+        return string;
+    }
 };
 
 // Do not edit below this line
